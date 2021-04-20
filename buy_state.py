8e5d5b8849dea83b8b2a587ee072adcfcc854d43
nikita <nikit0sik.sh@yandex.ru>
2021-04-21 01:09:42 +0300

    bugs fixed

--- buy_state.py
+++ buy_state.py
@@ -39,7 +39,7 @@ async def choose_adress(message: types.Message, state: FSMContext):
 
 async def choose_index(message: types.Message, state: FSMContext):
     if (not message.text.isdigit()): database.dataSetIndex(0, message.from_user.id)
-    database.dataSetIndex(int(message.text), message.from_user.id)
+    else: database.dataSetIndex(int(message.text), message.from_user.id)
     await message.answer(MESSEGES["Choose_contacts"], reply_markup=keyboards.getCancelKeyboard(),
                                disable_notification=True)
     await OrderParfume.next()
